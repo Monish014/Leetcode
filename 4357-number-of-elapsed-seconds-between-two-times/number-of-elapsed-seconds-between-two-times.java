67dba176e953476ae347eb56a3588dc16bf15ab7
Monish014 <monishmonis17@gmail.com>
2026-07-15 12:34:53 +0530

    Time: 3 ms (54.44%) | Memory: 44.9 MB (9.82%) - LeetSync

---
+++ 4357-number-of-elapsed-seconds-between-two-times/number-of-elapsed-seconds-between-two-times.java
@@ -0,0 +1,9 @@
+class Solution {
+    public int secondsBetweenTimes(String startTime, String endTime) {
+        String[] s=startTime.split(":");
+        String[] s1=endTime.split(":");
+        int a=Integer.parseInt(s[0])*3600+Integer.parseInt(s[1])*60+Integer.parseInt(s[2]);
+        int b=Integer.parseInt(s1[0])*3600+Integer.parseInt(s1[1])*60+Integer.parseInt(s1[2]);
+        return b-a;
+    }
+}
