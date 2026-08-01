1a6c6882921ac57a65945e1b56d4591bba97d10d
Monish014 <monishmonis17@gmail.com>
2026-08-01 15:50:34 +0530

    Time: 3 ms (95.62%) | Memory: 42.7 MB (24.80%) - LeetSync

---
+++ 2752-sum-multiples/sum-multiples.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int sumOfMultiples(int n) {
+        int num=0;
+        for(int i=1;i<=n;i++){
+            if(i%3==0 || i%5==0 || i%7==0){
+                num+=i;
+            }
+        }
+        return num;
+    }
+}
