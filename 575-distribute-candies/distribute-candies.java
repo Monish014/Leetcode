77c543a894b506a36ced3d6a8de1a61e9bf2d4d8
Monish014 <monishmonis17@gmail.com>
2026-08-26 23:37:11 +0530

    Time: 30 ms (91.92%) | Memory: 48.7 MB (16.37%) - LeetSync

---
+++ 575-distribute-candies/distribute-candies.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int distributeCandies(int[] candyType) {
+        Set<Integer> set=new HashSet<>();
+        for(int n:candyType){
+            set.add(n);
+        }
+        if(candyType.length/2>set.size()){
+            return set.size();
+        }
+        return candyType.length/2;
+    }
+}
